cef06482d15b867dbb0a9bd1c06d9c142734f367
Darko Stankovski <darko.itpro@gmail.com>
2019-02-04 00:08:03 +0100

    Switched tests to pytest.

--- tests/clean_classic/employee_data_extract_test.py
+++ tests/clean_classic/employee_data_extract_test.py
@@ -1,55 +1,46 @@
 #!/usr/bin/env python 
 
-import unittest as ut
 from clean_classic import birthday_greeting as bg
 
 
-class TestEmployeeDataExtract(ut.TestCase):
+def test_ok_dataset():
+    dataset = ["John, Doe, 12/07/1999, him@home.com"]
 
-    def test_ok_dataset(self):
-        dataset = ["John, Doe, 12/07/1999, him@home.com"]
-
-        self.assertTrue(len(list(bg.employees_birthiday_extract(dataset,
-                                                                '12/07'))) == 1)
-
-    def test_wrong_data_returns_no_value(self):
-        dataset = ["John, Doe, 12/07/1999"]
-
-        self.assertTrue(len(list(bg.employees_birthiday_extract(dataset,
-                                                                '12/07'))) == 0)
-
-        dataset = ["John, Doe, 12/07/1999, him@home.com, other"]
-
-        self.assertTrue(len(list(bg.employees_birthiday_extract(dataset,
-                                                                '12/07'))) == 0)
-
-    def test_no_current_birthday_returns_no_value(self):
-        dataset = ["John, Doe, 12/07/1999, him@home.com"]
-
-        self.assertTrue(len(list(bg.employees_birthiday_extract(dataset,
-                                                                '12/08'))) == 0)
-
-        dataset = ["John, Doe, 12/07/1999, him@home.com",
-                   "Jane, Doe, 12/08/1999, him@home.com"]
-
-        self.assertTrue(len(list(bg.employees_birthiday_extract(dataset,
-                                                                '12/08'))) == 1)
-
-    def test_returned_data_is_firstname_and_email(self):
-        dataset = ["John, Doe, 12/07/1999, him@home.com"]
-
-        first_data, second_data = next(bg.employees_birthiday_extract(dataset,
-                                                                      '12/07'))
-
-        self.assertEqual(first_data, 'John')
-        self.assertEqual(second_data, 'him@home.com')
-
-    def test_incorrect_date_format_result_in_no_value(self):
-        dataset = ["John, Doe, 12/7/1999, him@home.com"]
-
-        self.assertTrue(len(list(bg.employees_birthiday_extract(dataset,
-                                                                '12/07'))) == 0)
+    assert len(list(bg.employees_birthiday_extract(dataset, '12/07'))) == 1
 
 
-if __name__ == '__main__':
-    pass
+def test_wrong_data_returns_no_value():
+    dataset = ["John, Doe, 12/07/1999"]
+
+    assert len(list(bg.employees_birthiday_extract(dataset, '12/07'))) == 0
+
+    dataset = ["John, Doe, 12/07/1999, him@home.com, other"]
+
+    assert len(list(bg.employees_birthiday_extract(dataset, '12/07'))) == 0
+
+
+def test_no_current_birthday_returns_no_value():
+    dataset = ["John, Doe, 12/07/1999, him@home.com"]
+
+    assert len(list(bg.employees_birthiday_extract(dataset, '12/08'))) == 0
+
+    dataset = ["John, Doe, 12/07/1999, him@home.com",
+               "Jane, Doe, 12/08/1999, him@home.com"]
+
+    assert len(list(bg.employees_birthiday_extract(dataset, '12/08'))) == 1
+
+
+def test_returned_data_is_firstname_and_email():
+    dataset = ["John, Doe, 12/07/1999, him@home.com"]
+
+    first_data, second_data = next(bg.employees_birthiday_extract(dataset,
+                                                                  '12/07'))
+
+    assert first_data == 'John'
+    assert second_data == 'him@home.com'
+
+
+def test_incorrect_date_format_result_in_no_value():
+    dataset = ["John, Doe, 12/7/1999, him@home.com"]
+
+    assert len(list(bg.employees_birthiday_extract(dataset, '12/07'))) == 0
--- tests/python_fanatik/employee_data_extract_test.py
+++ tests/python_fanatik/employee_data_extract_test.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python 
 
-import unittest as ut
 from python_fanatik import birthday_greeting as bg
 
 
-class TestEmployeeDataExtract(ut.TestCase):
+def test_ok_dataset():
+    dataset = ["John", "Doe", "12/07/1999", "him@home.com"]
 
-    def test_ok_dataset(self):
-        dataset = ["John", "Doe", "12/07/1999", "him@home.com"]
-
-        self.assertTrue(bg.is_employee_birthday(dataset, '12/07'))
-
-    def test_wrong_data_returns_no_value(self):
-        dataset = ["John", "him@home.com"]
-
-        self.assertFalse(bg.is_employee_birthday(dataset, '12/07'))
-
-    def test_no_current_birthday(self):
-        dataset = ["John", "Doe", "12/07/1999", "him@home.com"]
-
-        self.assertFalse(bg.is_employee_birthday(dataset, '12/08'))
-
-    def test_incorrect_date_format_result_in_false(self):
-        dataset = ["John, Doe, 12/7/1999, him@home.com"]
-
-        self.assertFalse(bg.is_employee_birthday(dataset, '12/07'))
+    assert bg.is_employee_birthday(dataset, '12/07')
 
 
-if __name__ == '__main__':
-    pass
+def test_wrong_data_returns_no_value():
+    dataset = ["John", "him@home.com"]
+
+    assert bg.is_employee_birthday(dataset, '12/07') is False
+
+
+def test_no_current_birthday():
+    dataset = ["John", "Doe", "12/07/1999", "him@home.com"]
+
+    assert bg.is_employee_birthday(dataset, '12/08') is False
+
+
+def test_incorrect_date_format_result_in_false():
+    dataset = ["John, Doe, 12/7/1999, him@home.com"]
+
+    assert bg.is_employee_birthday(dataset, '12/07') is False
